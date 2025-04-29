ea91f1f375616987a76385d120090bc69cb5d53d
Topher Ludlow <topherludlow@protonmail.com>
2025-04-29 13:48:36 -0700

    Changed title

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -214,12 +214,11 @@ function Photos() {
               src={image.src}
               alt={image.alt}
               sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover rendering-crisp-edges"
+              className="absolute inset-0 h-full w-full object-cover"
               width={288}  
               height={320}
               quality={90}
               unoptimized
-              loading="eager"
             />
           </div>
         ))}
@@ -235,7 +234,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software developer.
+            Software Developer
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I am a results-driven Full Stack Software Developer with expertise in NextJS, ExpressJS, PostgreSQL and RESTful APIs, specializing in creating robust applications that seamlessly integrate across web and mobile platforms. My experience ranges from architecting music streaming platforms and e-commerce solutions to volunteering for humanitarian aid logistics in Ukraine, showcasing my commitment to leveraging technology for meaningful impact. With a military background as a Signal Support Systems Specialist and formal education from Flatiron School, I bring a unique blend of technical discipline and creative problem-solving to every project. I excel at translating complex business requirements into elegant technical solutions, focusing on performance, security, and scalability while collaborating effectively across diverse teams. Even during my personal time, I am immersed in code, constantly exploring new technologies and building side projects that fuel my passion for development while sharpening the skills I bring to every professional engagement.
